3243c09e189f906a22024e3814e325507ec5540a
anddyf <ferreiraanddy@gmail.com>
2019-10-16 00:51:08 -0400

    2 out of 3

--- src/callbacker.js
+++ src/callbacker.js
@@ -1,5 +1,23 @@
-const syncCallbacker = null;
+const syncCallbacker = (arg1,arg2) => {
+if(typeof arg1 !== 'function' || typeof arg2 !== 'function'  ){
+    throw ''
+}
+else{
+  let result = arg2(arg1())
+  return result
+}
+};
 
-const asyncCallbacker = null;
+const asyncCallbacker = (arg1,arg2) =>{
+if(typeof arg1 !== 'function' || typeof arg2 !== 'function'  ){
+    throw ''
+}
+let data = ''
+let done = (data) =>{
+    arg2(data, done)
+}
+
+arg1(data,done)
+}
 
 module.exports = { syncCallbacker, asyncCallbacker };
--- src/promise.js
+++ src/promise.js
@@ -1,6 +1,15 @@
-const basicPromise = null;
-const funcPromise = () => null;
-const chainedPromise = prom => null;
-const rejectedPromise = () => null;
+const basicPromise = new Promise(function(resolve, reject) {
+    resolve('basicPromise')
+})
+const funcPromise = () => new Promise(function(resolve, reject) {
+    resolve('funcPromise')
+})
+const chainedPromise = prom => new Promise(function(resolve,reject){
+    resolve(prom)
+}).then(prom)
+
+const rejectedPromise = () => new Promise(function(resolve,reject){
+     throw Error("rejectedPromise")
+})
 
 module.exports = { basicPromise, chainedPromise, rejectedPromise, funcPromise };
